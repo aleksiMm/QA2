b8ab72d54432dfa37d16927dc8c629ee4d7fd075
a.sviridovics <a.sviridovics@googlemail.com>
2022-04-01 13:43:12 +0300

    Done

--- src/test/java/Lecture7/AliTest.java
+++ src/test/java/Lecture7/AliTest.java
@@ -1,6 +1,7 @@
 package Lecture7;
 
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.chrome.ChromeDriver;
 import static org.assertj.core.api.Assertions.assertThat;
 import org.openqa.selenium.support.ui.Select;
@@ -9,10 +10,24 @@ public class AliTest {
 
     @Test
     public void aliTatoo() {
+
         String driverPath = "C:\\Users\\a.sviridovics\\IdeaProjects\\QA2\\src\\main\\resources\\chromedriver.exe";
         System.setProperty("webdriver.chrome.driver", driverPath);
         ChromeDriver driver = new ChromeDriver();
         driver.get("https://www.aliexpress.com/");
+
+        driver.findElement(By.xpath("//img[@class='_24EHh']")).click();
+        driver.findElement(By.xpath("//img[@class='btn-close']")).click();
+        driver.findElement(By.xpath("//img[@class='close-btn']")).click();
+        driver.findElement(By.id("search-key")).sendKeys("tattoo");
+        String searchingFor = driver.findElement(By.id("search-key")).getAttribute("value");
+        assertThat(searchingFor).isEqualTo("tattoo");
+        driver.findElement(By.xpath("//input[@class = 'search-button']")).click();
+        driver.findElement(By.xpath("//input[@placeholder= 'min']")).sendKeys("10");
+        driver.findElement(By.xpath("//input[@placeholder= 'max']")).sendKeys("20");
+        driver.findElement(By.xpath("//a[@class= 'ui-button narrow-go']")).click();
+
+
         try {
             Thread.sleep(5000);
         } catch (InterruptedException e) {
