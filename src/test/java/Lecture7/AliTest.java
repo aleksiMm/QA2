6f9a14140dc9452b3f8d5877b36691f590d213bd
a.sviridovics <a.sviridovics@googlemail.com>
2022-04-01 13:52:34 +0300

    * minor updates

--- src/test/java/Lecture7/AliTest.java
+++ src/test/java/Lecture7/AliTest.java
@@ -4,7 +4,6 @@ import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.chrome.ChromeDriver;
 import static org.assertj.core.api.Assertions.assertThat;
-import org.openqa.selenium.support.ui.Select;
 
 public class AliTest {
 
@@ -15,7 +14,7 @@ public class AliTest {
         System.setProperty("webdriver.chrome.driver", driverPath);
         ChromeDriver driver = new ChromeDriver();
         driver.get("https://www.aliexpress.com/");
-
+        driver.manage().window().maximize();
         driver.findElement(By.xpath("//img[@class='_24EHh']")).click();
         driver.findElement(By.xpath("//img[@class='btn-close']")).click();
         driver.findElement(By.xpath("//img[@class='close-btn']")).click();
@@ -26,7 +25,6 @@ public class AliTest {
         driver.findElement(By.xpath("//input[@placeholder= 'min']")).sendKeys("10");
         driver.findElement(By.xpath("//input[@placeholder= 'max']")).sendKeys("20");
         driver.findElement(By.xpath("//a[@class= 'ui-button narrow-go']")).click();
-
 
         try {
             Thread.sleep(5000);
